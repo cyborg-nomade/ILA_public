31a7442e84e33d1ce149d0c7b4b7e202b1813201
Uriel Fiori <uriel.fiori@gmail.com>
2022-03-16 15:53:41 -0300

    added API docs page and streamlined the project

--- CPTM.ILA.Web/Models/ILAContext.cs
+++ CPTM.ILA.Web/Models/ILAContext.cs
@@ -1,9 +1,40 @@
-﻿//-----------------------------------------------------------------------------------------------------------------------
-// <CPTM>
-//
-//  Este codigo foi gerado automaticamente por um template e não pode ser alterado.
-//
-// <CPTM>
-//-----------------------------------------------------------------------------------------------------------------------
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
 
+namespace CPTM.ILA.Web.Models
+{
+    public partial class ILAContext : DbContext
+    {
+        public ILAContext() : base("name=ILAContext")
+        {
+        }
 
+        public virtual DbSet<ILA_CONFIGURACAO> ILA_CONFIGURACAO { get; set; }
+        public virtual DbSet<ILA_USUARIO_PREFERENCIA> ILA_USUARIO_PREFERENCIA { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ILA_CONFIGURACAO>()
+                .Property(e => e.PARAMETRO)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<ILA_CONFIGURACAO>()
+                .Property(e => e.TX_DESCRICAO)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<ILA_CONFIGURACAO>()
+                .Property(e => e.VALOR)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<ILA_USUARIO_PREFERENCIA>()
+                .Property(e => e.TX_CATEGORIA)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<ILA_USUARIO_PREFERENCIA>()
+                .Property(e => e.TX_VALOR)
+                .IsUnicode(false);
+        }
+    }
+}
