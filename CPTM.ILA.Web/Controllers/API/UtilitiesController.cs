9366933c62193e9705547ac2e3984a79d6068ff4
Uriel Fiori <uriel.fiori@gmail.com>
2022-03-29 14:08:35 -0300

    countries endpoint to feed the frontend

---
+++ CPTM.ILA.Web/Controllers/API/UtilitiesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using CPTM.ILA.Web.Models.CaseHelpers.Enums;
+
+namespace CPTM.ILA.Web.Controllers.API
+{
+    [RoutePrefix("api/utilities")]
+    public class UtilitiesController : ApiController
+    {
+        [Route("countries")]
+        [Authorize]
+        [HttpGet]
+        public HttpResponseMessage GetCountries()
+        {
+            var countries = Pais.ListaDePaises();
+            return Request.CreateResponse(HttpStatusCode.OK, new { countries });
+        }
+    }
+}
--- CPTM.ILA.Web/Models/CaseHelpers/ItemTransferenciaInternacional.cs
+++ CPTM.ILA.Web/Models/CaseHelpers/ItemTransferenciaInternacional.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using CPTM.ILA.Web.Models.CaseHelpers.Enums;
 
 namespace CPTM.ILA.Web.Models.CaseHelpers
@@ -12,5 +14,17 @@ namespace CPTM.ILA.Web.Models.CaseHelpers
         public string Pais { get; set; }
         public string DadosTransferidos { get; set; }
         [MaxLength(250)] public string TipoGarantia { get; set; }
+
+        public ItemTransferenciaInternacional Rectify()
+        {
+            if (!Enums.Pais.IsPais(Pais))
+            {
+                Pais = Enums.Pais.ListaDePaises()
+                    .OrderBy(p => string.CompareOrdinal(p, Pais))
+                    .First();
+            }
+
+            return this;
+        }
     }
 }
