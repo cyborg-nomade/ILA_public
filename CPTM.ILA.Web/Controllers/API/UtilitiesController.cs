11ddff853ea49de176e49352397dc3bd94412d32
Uriel Fiori <uriel.fiori@gmail.com>
2022-03-31 18:48:40 -0300

    returning all systems

--- CPTM.ILA.Web/Controllers/API/UtilitiesController.cs
+++ CPTM.ILA.Web/Controllers/API/UtilitiesController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using CPTM.ILA.Web.Models;
+using CPTM.ILA.Web.Models.AccessControl.VIEWS;
 using CPTM.ILA.Web.Models.CaseHelpers.Enums;
 
 namespace CPTM.ILA.Web.Controllers.API
@@ -15,6 +18,14 @@ namespace CPTM.ILA.Web.Controllers.API
     [RoutePrefix("api/utilities")]
     public class UtilitiesController : ApiController
     {
+        private ILAContext _context;
+
+        /// <inheritdoc />
+        public UtilitiesController()
+        {
+            _context = new ILAContext();
+        }
+
         /// <summary>
         /// Retorna a lista completa de países do mundo, em português.
         /// </summary>
@@ -26,8 +37,36 @@ namespace CPTM.ILA.Web.Controllers.API
         [HttpGet]
         public HttpResponseMessage GetCountries()
         {
-            var countries = Pais.ListaDePaises();
-            return Request.CreateResponse(HttpStatusCode.OK, new { countries });
+            try
+            {
+                var countries = Pais.ListaDePaises();
+                return Request.CreateResponse(HttpStatusCode.OK, new { countries });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError,
+                    new { message = "Algo deu errado no servidor. Reporte ao suporte técnico." });
+            }
+        }
+
+        [Route("systems")]
+        [Authorize]
+        [HttpGet]
+        public async Task<HttpResponseMessage> GetSystems()
+        {
+            try
+            {
+                var systems = await _context.ILA_VW_CATPROD_PRODUTO.ToListAsync();
+
+                return Request.CreateResponse(HttpStatusCode.OK, new { systems });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError,
+                    new { message = "Algo deu errado no servidor. Reporte ao suporte técnico." });
+            }
         }
     }
 }
