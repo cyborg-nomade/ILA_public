a4338bad22b7c69f5e041166cae7270729e96e2b
Uriel Fiori <uriel.fiori@gmail.com>
2022-04-12 10:39:49 -0300

    section 13 working fine

--- CPTM.ILA.Frontend/src/cases/components/form-items/Section13FormRow.tsx
+++ CPTM.ILA.Frontend/src/cases/components/form-items/Section13FormRow.tsx
@@ -1,134 +1,123 @@
 import React, { useState } from "react";
 
-import { useFormikContext, getIn } from "formik";
+import { useFormikContext, FieldArray } from "formik";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 import { Case } from "../../../shared/models/cases.model";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
+import { emptyItemTransferenciaInternacional } from "../../../shared/models/case-helpers/case-helpers.model";
+import CreateCommentBox from "./../../../threads-comments/components/CreateCommentBox";
+import { CaseIndexDictionary } from "../../../shared/models/case-index.dictionary";
+import Section13FormRowSub from "./Section13FormRowSub";
 
-const Section13FormRow = (props: {
-  disabled: boolean;
-  name: string;
-  className: string;
-}) => {
-  const { values, touched, errors, handleChange, handleBlur, setFieldValue } =
-    useFormikContext<Case>();
+const Section13FormRow = (props: { countries: string[] }) => {
+  const { values, setFieldValue } = useFormikContext<Case>();
 
-  const [nomeOrganizacao, setNomeOrganizacao] = useState(
-    getIn(values, `${props.name}.nomeOrganizacao`)
-  );
-  const [pais, setPais] = useState(getIn(values, `${props.name}.pais`));
-  const [dadosTransferidos, setDadosTransferidos] = useState(
-    getIn(values, `${props.name}.dadosTransferidos`)
-  );
+  const [trata, setTrata] = useState(false);
 
-  const handleChangeNomeOrganizacao = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setNomeOrganizacao(event.currentTarget.value);
-  };
-  const handleBlurNomeOrganizacao = (
-    event: React.FocusEvent<HTMLInputElement>
-  ) => {
-    handleBlur(event);
-    setFieldValue(`${props.name}.nomeOrganizacao`, nomeOrganizacao, true);
-  };
-
-  const handleChangePais = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPais(event.currentTarget.value);
-  };
-  const handleBlurPais = (event: React.FocusEvent<HTMLInputElement>) => {
-    handleBlur(event);
-    setFieldValue(`${props.name}.pais`, pais, true);
-  };
-
-  const handleChangeDadosTransferidos = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setDadosTransferidos(event.currentTarget.value);
-  };
-  const handleBlurDadosTransferidos = (
-    event: React.FocusEvent<HTMLInputElement>
-  ) => {
-    handleBlur(event);
-    setFieldValue(`${props.name}.dadosTransferidos`, dadosTransferidos, true);
+  const handleTrataRadio = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.currentTarget.value === "SIM") {
+      setTrata(true);
+    } else {
+      setTrata(false);
+      setFieldValue("transferenciaInternacional", []);
+    }
   };
 
   return (
-    <Row className={props.className}>
-      <Col>
-        <Form.Control
-          disabled={props.disabled}
-          type="text"
-          name={`${props.name}.nomeOrganizacao`}
-          value={nomeOrganizacao}
-          onChange={handleChangeNomeOrganizacao}
-          onBlur={handleBlurNomeOrganizacao}
-          isValid={
-            getIn(touched, `${props.name}.nomeOrganizacao`) &&
-            !getIn(errors, `${props.name}.nomeOrganizacao`)
-          }
-          isInvalid={!!getIn(errors, `${props.name}.nomeOrganizacao`)}
-        />
-      </Col>
-      <Col>
-        <Form.Control
-          disabled={props.disabled}
-          type="text"
-          name={`${props.name}.pais`}
-          value={pais}
-          onChange={handleChangePais}
-          onBlur={handleBlurPais}
-          isValid={
-            getIn(touched, `${props.name}.pais`) &&
-            !getIn(errors, `${props.name}.pais`)
-          }
-          isInvalid={!!getIn(errors, `${props.name}.pais`)}
-        />
-      </Col>
-      <Col>
-        <Form.Control
-          disabled={props.disabled}
-          type="text"
-          name={`${props.name}.dadosTransferidos`}
-          value={dadosTransferidos}
-          onChange={handleChangeDadosTransferidos}
-          onBlur={handleBlurDadosTransferidos}
-          isValid={
-            getIn(touched, `${props.name}.dadosTransferidos`) &&
-            !getIn(errors, `${props.name}.dadosTransferidos`)
-          }
-          isInvalid={!!getIn(errors, `${props.name}.dadosTransferidos`)}
-        />
-      </Col>
-      <Col>
-        <Form.Select
-          disabled={props.disabled}
-          name={`${props.name}.tipoGarantia`}
-          value={getIn(values, `${props.name}.tipoGarantia`)}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          isValid={
-            getIn(touched, `${props.name}.tipoGarantia`) &&
-            !getIn(errors, `${props.name}.tipoGarantia`)
-          }
-          isInvalid={!!getIn(errors, `${props.name}.tipoGarantia`)}
-        >
-          {Object.values(tipoGarantiaTranferenciaInternacional).map((tip) => (
-            <option value={tip} key={tip}>
-              {tip}
-            </option>
-          ))}
-        </Form.Select>
-      </Col>
-    </Row>
+    <React.Fragment>
+      <Row>
+        <Col></Col>
+        <Col className="d-grid justify-content-center">
+          <Form.Check
+            type="radio"
+            name="trata"
+            required
+            label="Sim"
+            value="SIM"
+            onChange={handleTrataRadio}
+          />
+          <Form.Check
+            type="radio"
+            name="trata"
+            required
+            inline
+            label="Não"
+            value="NÃO"
+            onChange={handleTrataRadio}
+          />
+        </Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col lg={1}>
+          <Row>
+            <CreateCommentBox
+              item={CaseIndexDictionary.transferenciaInternacional}
+            />
+          </Row>
+        </Col>
+      </Row>
+      <FieldArray
+        name="transferenciaInternacional"
+        render={(arrayHelpers) => (
+          <React.Fragment>
+            {values.transferenciaInternacional &&
+            values.transferenciaInternacional.length > 0 ? (
+              values.transferenciaInternacional.map((item, index) => (
+                <React.Fragment key={index}>
+                  <Section13FormRowSub
+                    className={`mb-3 pt-2 pb-2 ${
+                      index % 2 === 0 ? "bg-primary bg-opacity-10" : ""
+                    }`}
+                    name={`transferenciaInternacional[${index}]`}
+                    countries={props.countries}
+                  />
+                  <Row className="justify-content-center">
+                    <ButtonGroup as={Col} className="mt-1 mb-3" lg={2}>
+                      <Button
+                        variant="primary"
+                        onClick={() =>
+                          arrayHelpers.push(
+                            emptyItemTransferenciaInternacional()
+                          )
+                        }
+                      >
+                        +
+                      </Button>
+                      <Button
+                        variant="danger"
+                        onClick={() => arrayHelpers.remove(index)}
+                      >
+                        -
+                      </Button>
+                    </ButtonGroup>
+                  </Row>
+                </React.Fragment>
+              ))
+            ) : (
+              <Row className="justify-content-center">
+                <ButtonGroup as={Col} className="mt-1 mb-3" lg={2}>
+                  <Button
+                    variant="primary"
+                    disabled={!trata}
+                    onClick={() =>
+                      arrayHelpers.push(emptyItemTransferenciaInternacional())
+                    }
+                  >
+                    +
+                  </Button>
+                </ButtonGroup>
+              </Row>
+            )}
+          </React.Fragment>
+        )}
+      />
+    </React.Fragment>
   );
 };
 
 export default Section13FormRow;
-function tipoGarantiaTranferenciaInternacional(
-  tipoGarantiaTranferenciaInternacional: any
-) {
-  throw new Error("Function not implemented.");
-}
