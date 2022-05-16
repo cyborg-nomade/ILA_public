89ce4056cde5cdc836b6fbcc153f8715bd9e2649
Uriel Fiori <uriel.fiori@gmail.com>
2022-05-16 12:55:07 -0300

    code cleanup pt 3 + selectors now have active status + threads dashboard reactivated to show cases by status

--- CPTM.ILA.Frontend/src/threads-comments/components/CreateCommentBox.tsx
+++ CPTM.ILA.Frontend/src/threads-comments/components/CreateCommentBox.tsx
@@ -1,16 +1,17 @@
+import React, { useContext, useState } from "react";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Popover from "react-bootstrap/Popover";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { AiFillCaretDown } from "react-icons/ai";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import React, { useContext, useState } from "react";
-import { AuthContext } from "../../shared/context/auth-context";
-import { useHttpClient } from "../../shared/hooks/http-hook";
-import { CommentDTO } from "../../shared/models/DTOs/comment-dto";
 import Spinner from "react-bootstrap/Spinner";
 import Alert from "react-bootstrap/Alert";
+import { AiFillCaretDown } from "react-icons/ai";
+
+import { CommentDTO } from "../../shared/models/DTOs/comment-dto";
+import { AuthContext } from "../../shared/context/auth-context";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const CreateCommentBox = (props: {
   item: { number: string; title: string };
