f463c09cc32ca2e231742598865cdb49e164694f
Uriel Fiori <uriel.fiori@gmail.com>
2022-06-07 11:57:07 -0300

    Tela Solicitação Acesso a Grupos => Modal com confirmação, on dismiss redirecionar pra página de login + Login => mostrar msgs de erro acima do form

--- CPTM.ILA.Frontend/src/users/pages/Login.tsx
+++ CPTM.ILA.Frontend/src/users/pages/Login.tsx
@@ -18,171 +18,183 @@ import { AuthContext } from "../../shared/context/auth-context";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const schema = yup.object().shape({
-  username: yup.string().required(),
-  password: yup.string().required(),
+    username: yup.string().required(),
+    password: yup.string().required(),
 });
 
 const initialValues: AuthUser = {
-  username: "",
-  password: "",
+    username: "",
+    password: "",
 };
 
 const Login = () => {
-  const { login, changeComiteMember } = useContext(AuthContext);
+    const { login, changeComiteMember } = useContext(AuthContext);
 
-  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-  let navigate = useNavigate();
+    let navigate = useNavigate();
 
-  const submitLoginHandler = async (user: AuthUser) => {
-    try {
-      const responseData = await sendRequest(
-        `${process.env.REACT_APP_CONNSTR}/users/login`,
-        "POST",
-        JSON.stringify(user),
-        {
-          "Content-Type": "application/json",
+    const submitLoginHandler = async (user: AuthUser) => {
+        try {
+            const responseData = await sendRequest(
+                `${process.env.REACT_APP_CONNSTR}/users/login`,
+                "POST",
+                JSON.stringify(user),
+                {
+                    "Content-Type": "application/json",
+                }
+            );
+            console.log("login response data:", responseData);
+
+            const receivedUser: User = responseData.user;
+            const receivedAreaTratamentoDados: AgenteTratamento =
+                responseData.areaTratamentoDados;
+            const isDeveloper = responseData.isDeveloper;
+
+            login(
+                receivedUser,
+                isDeveloper,
+                responseData.token,
+                receivedUser.originGroup,
+                receivedAreaTratamentoDados
+            );
+
+            if (receivedUser.isDPO) {
+                const responseDataComiteMembers = await sendRequest(
+                    `${process.env.REACT_APP_CONNSTR}/users/comite-members`,
+                    undefined,
+                    undefined,
+                    {
+                        "Content-Type": "application/json",
+                        Authorization: "Bearer " + responseData.token,
+                    }
+                );
+
+                const loadedMembers: ComiteMember[] =
+                    responseDataComiteMembers.comiteMembers;
+                console.log("loadedMembers", loadedMembers);
+                changeComiteMember(loadedMembers[0]);
+            }
+
+            navigate(`/${receivedUser.id}/`);
+        } catch (error) {
+            console.log(error);
         }
-      );
-      console.log("login response data:", responseData);
+    };
 
-      const receivedUser: User = responseData.user;
-      const receivedAreaTratamentoDados: AgenteTratamento =
-        responseData.areaTratamentoDados;
-      const isDeveloper = responseData.isDeveloper;
+    const registerFormHandler = () => {
+        navigate(`/request-access`);
+    };
 
-      login(
-        receivedUser,
-        isDeveloper,
-        responseData.token,
-        receivedUser.originGroup,
-        receivedAreaTratamentoDados
-      );
-
-      if (receivedUser.isDPO) {
-        const responseDataComiteMembers = await sendRequest(
-          `${process.env.REACT_APP_CONNSTR}/users/comite-members`,
-          undefined,
-          undefined,
-          {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + responseData.token,
-          }
+    if (isLoading) {
+        return (
+            <Row className="justify-content-center">
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            </Row>
         );
-
-        const loadedMembers: ComiteMember[] =
-          responseDataComiteMembers.comiteMembers;
-        console.log("loadedMembers", loadedMembers);
-        changeComiteMember(loadedMembers[0]);
-      }
-
-      navigate(`/${receivedUser.id}/`);
-    } catch (error) {
-      console.log(error);
     }
-  };
 
-  const registerFormHandler = () => {
-    navigate(`/request-access`);
-  };
-
-  if (isLoading) {
     return (
-      <Row className="justify-content-center">
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-      </Row>
+        <React.Fragment>
+            {error && (
+                <Row
+                    className="justify-content-center mx-auto"
+                    style={{ width: "28rem" }}
+                >
+                    <Alert variant="danger" onClose={clearError} dismissible>
+                        {error}
+                    </Alert>
+                </Row>
+            )}
+            <Formik
+                validationSchema={schema}
+                onSubmit={submitLoginHandler}
+                initialValues={initialValues}
+            >
+                {({
+                    handleSubmit,
+                    handleChange,
+                    handleBlur,
+                    values,
+                    touched,
+                    isValid,
+                    errors,
+                    dirty,
+                }) => (
+                    <Form noValidate onSubmit={handleSubmit}>
+                        <Card className="mx-auto" style={{ width: "28rem" }}>
+                            <Card.Title className="pt-3 px-3">Login</Card.Title>
+                            <Card.Body>
+                                <Row className="mb-3">
+                                    <Form.Group
+                                        as={Col}
+                                        controlId="validationFormik01"
+                                    >
+                                        <Form.Label>Usuário</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            name="username"
+                                            value={values.username}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            isValid={
+                                                touched.username &&
+                                                !errors.username
+                                            }
+                                            isInvalid={!!errors.username}
+                                        />
+                                        <Form.Control.Feedback type="invalid">
+                                            Esse campo é obrigatório
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                </Row>
+                                <Row className="mb-3">
+                                    <Form.Group
+                                        as={Col}
+                                        controlId="validationFormik02"
+                                    >
+                                        <Form.Label>Senha</Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            name="password"
+                                            value={values.password}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            isValid={
+                                                touched.password &&
+                                                !errors.password
+                                            }
+                                            isInvalid={!!errors.password}
+                                        />
+                                        <Form.Control.Feedback type="invalid">
+                                            Esse campo é obrigatório
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                </Row>
+                                <Button
+                                    type="submit"
+                                    className="float-end mt-3"
+                                    disabled={!(isValid && dirty)}
+                                >
+                                    Login
+                                </Button>
+                                <Button
+                                    type="button"
+                                    className="float-start mt-3"
+                                    variant="success"
+                                    onClick={registerFormHandler}
+                                >
+                                    Solicitar Acesso
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Form>
+                )}
+            </Formik>
+        </React.Fragment>
     );
-  }
-
-  return (
-    <React.Fragment>
-      <Formik
-        validationSchema={schema}
-        onSubmit={submitLoginHandler}
-        initialValues={initialValues}
-      >
-        {({
-          handleSubmit,
-          handleChange,
-          handleBlur,
-          values,
-          touched,
-          isValid,
-          errors,
-          dirty,
-        }) => (
-          <Form noValidate onSubmit={handleSubmit}>
-            <Card className="mx-auto" style={{ width: "28rem" }}>
-              <Card.Title className="pt-3 px-3">Login</Card.Title>
-              <Card.Body>
-                <Row className="mb-3">
-                  <Form.Group as={Col} controlId="validationFormik01">
-                    <Form.Label>Usuário</Form.Label>
-                    <Form.Control
-                      type="text"
-                      name="username"
-                      value={values.username}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      isValid={touched.username && !errors.username}
-                      isInvalid={!!errors.username}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      Esse campo é obrigatório
-                    </Form.Control.Feedback>
-                  </Form.Group>
-                </Row>
-                <Row className="mb-3">
-                  <Form.Group as={Col} controlId="validationFormik02">
-                    <Form.Label>Senha</Form.Label>
-                    <Form.Control
-                      type="password"
-                      name="password"
-                      value={values.password}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      isValid={touched.password && !errors.password}
-                      isInvalid={!!errors.password}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      Esse campo é obrigatório
-                    </Form.Control.Feedback>
-                  </Form.Group>
-                </Row>
-                <Button
-                  type="submit"
-                  className="float-end mt-3"
-                  disabled={!(isValid && dirty)}
-                >
-                  Login
-                </Button>
-                <Button
-                  type="button"
-                  className="float-start mt-3"
-                  variant="success"
-                  onClick={registerFormHandler}
-                >
-                  Solicitar Acesso
-                </Button>
-              </Card.Body>
-            </Card>
-          </Form>
-        )}
-      </Formik>
-      {error && (
-        <Row
-          className="justify-content-center mx-auto"
-          style={{ width: "28rem" }}
-        >
-          <Alert variant="danger" onClose={clearError} dismissible>
-            {error}
-          </Alert>
-        </Row>
-      )}
-    </React.Fragment>
-  );
 };
 
 export default Login;
