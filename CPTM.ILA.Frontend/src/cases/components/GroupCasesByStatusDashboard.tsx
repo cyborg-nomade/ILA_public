554066e7894ad0ca4e867179e9f127069d2fd4cf
Uriel Fiori <uriel.fiori@gmail.com>
2022-06-07 12:01:53 -0300

    - Tela inicial => colocar botão de ajuda no rodapé

--- CPTM.ILA.Frontend/src/cases/components/GroupCasesByStatusDashboard.tsx
+++ CPTM.ILA.Frontend/src/cases/components/GroupCasesByStatusDashboard.tsx
@@ -12,129 +12,118 @@ import { AuthContext } from "../../shared/context/auth-context";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const GroupCasesByStatusDashboard = () => {
-  const [concluidos, setConcluidos] = useState(0);
-  const [emPreenchimento, setEmPreenchimento] = useState(0);
-  const [pendenteAprovacao, setPendenteAprovacao] = useState(0);
+    const [concluidos, setConcluidos] = useState(0);
+    const [emPreenchimento, setEmPreenchimento] = useState(0);
+    const [pendenteAprovacao, setPendenteAprovacao] = useState(0);
 
-  const { token, currentGroup } = useContext(AuthContext);
+    const { token, currentGroup } = useContext(AuthContext);
 
-  const { isLoading, error, isWarning, sendRequest, clearError } =
-    useHttpClient();
+    const { isLoading, error, isWarning, sendRequest, clearError } =
+        useHttpClient();
 
-  useEffect(() => {
-    const getGroupCaseTotals = async () => {
-      try {
-        const responseData = await sendRequest(
-          `${process.env.REACT_APP_CONNSTR}/cases/group/${currentGroup.id}/status/totals`,
-          undefined,
-          undefined,
-          {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + token,
-          }
+    useEffect(() => {
+        const getGroupCaseTotals = async () => {
+            try {
+                const responseData = await sendRequest(
+                    `${process.env.REACT_APP_CONNSTR}/cases/group/${currentGroup.id}/status/totals`,
+                    undefined,
+                    undefined,
+                    {
+                        "Content-Type": "application/json",
+                        Authorization: "Bearer " + token,
+                    }
+                );
+
+                const loadedTotals: StatusTotals[] = responseData.totals;
+                console.log("groupCase loadedTotals: ", loadedTotals);
+
+                loadedTotals.sort((a, b) => (a.nome > b.nome ? 1 : -1));
+                console.log("groupCase loadedTotals sorted: ", loadedTotals);
+
+                if (loadedTotals.length !== 0) {
+                    loadedTotals.map((lt) => {
+                        if (lt.nome === "Concluído") {
+                            setConcluidos(lt.quantidadeByStatus);
+                        }
+                        if (lt.nome === "Em Preenchimento") {
+                            setEmPreenchimento(lt.quantidadeByStatus);
+                        }
+                        if (lt.nome === "Pendente Aprovação") {
+                            setPendenteAprovacao(lt.quantidadeByStatus);
+                        }
+                        return lt;
+                    });
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        };
+
+        getGroupCaseTotals().catch((error) => {
+            console.log(error);
+        });
+
+        return () => {
+            setConcluidos(0);
+            setEmPreenchimento(0);
+            setPendenteAprovacao(0);
+        };
+    }, [sendRequest, token, currentGroup.id]);
+
+    if (isLoading) {
+        return (
+            <Row className="justify-content-center">
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            </Row>
         );
+    }
 
-        const loadedTotals: StatusTotals[] = responseData.totals;
-        console.log("groupCase loadedTotals: ", loadedTotals);
-
-        loadedTotals.sort((a, b) => (a.nome > b.nome ? 1 : -1));
-        console.log("groupCase loadedTotals sorted: ", loadedTotals);
-
-        if (loadedTotals.length !== 0) {
-          loadedTotals.map((lt) => {
-            if (lt.nome === "Concluído") {
-              setConcluidos(lt.quantidadeByStatus);
-            }
-            if (lt.nome === "Em Preenchimento") {
-              setEmPreenchimento(lt.quantidadeByStatus);
-            }
-            if (lt.nome === "Pendente Aprovação") {
-              setPendenteAprovacao(lt.quantidadeByStatus);
-            }
-            return lt;
-          });
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    };
-
-    getGroupCaseTotals().catch((error) => {
-      console.log(error);
-    });
-
-    return () => {
-      setConcluidos(0);
-      setEmPreenchimento(0);
-      setPendenteAprovacao(0);
-    };
-  }, [sendRequest, token, currentGroup.id]);
-
-  if (isLoading) {
     return (
-      <Row className="justify-content-center">
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-      </Row>
+        <React.Fragment>
+            {error && (
+                <Alert
+                    variant={isWarning ? "warning" : "danger"}
+                    onClose={clearError}
+                    dismissible
+                >
+                    {error}
+                </Alert>
+            )}
+            <Row>
+                <h3 className="mb-4">Quantitativo dos Processos</h3>
+            </Row>
+            <Row>
+                <CardGroup>
+                    <Card border="danger">
+                        <Card.Header>Pendente Aprovação</Card.Header>
+                        <Card.Body>
+                            <h1 className="text-center">
+                                <Badge bg="danger">{pendenteAprovacao}</Badge>
+                            </h1>
+                        </Card.Body>
+                    </Card>
+                    <Card border="secondary">
+                        <Card.Header>Concluídos</Card.Header>
+                        <Card.Body>
+                            <h1 className="text-center">
+                                <Badge bg="secondary">{concluidos}</Badge>
+                            </h1>
+                        </Card.Body>
+                    </Card>
+                    <Card border="warning">
+                        <Card.Header>Em Preenchimento</Card.Header>
+                        <Card.Body>
+                            <h1 className="text-center">
+                                <Badge bg="warning">{emPreenchimento}</Badge>
+                            </h1>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+            </Row>
+        </React.Fragment>
     );
-  }
-
-  return (
-    <React.Fragment>
-      {error && (
-        <Alert
-          variant={isWarning ? "warning" : "danger"}
-          onClose={clearError}
-          dismissible
-        >
-          {error}
-        </Alert>
-      )}
-      <Row>
-        <h3 className="mb-4">Quantitativo dos Processos</h3>
-      </Row>
-      <Row>
-        <CardGroup>
-          <Card border="danger">
-            <Card.Header>Pendente Aprovação</Card.Header>
-            <Card.Body>
-              <h1 className="text-center">
-                <Badge bg="danger">{pendenteAprovacao}</Badge>
-              </h1>
-            </Card.Body>
-          </Card>
-          <Card border="secondary">
-            <Card.Header>Concluídos</Card.Header>
-            <Card.Body>
-              <h1 className="text-center">
-                <Badge bg="secondary">{concluidos}</Badge>
-              </h1>
-            </Card.Body>
-          </Card>
-          <Card border="warning">
-            <Card.Header>Em Preenchimento</Card.Header>
-            <Card.Body>
-              <h1 className="text-center">
-                <Badge bg="warning">{emPreenchimento}</Badge>
-              </h1>
-            </Card.Body>
-          </Card>
-        </CardGroup>
-      </Row>
-      <Row>
-        <Button
-          variant="success"
-          className="mt-4 mx-auto"
-          style={{ width: "8rem", height: "2.5rem" }}
-          href="https://www.cptm.sp.gov.br/LGPD/Paginas/Perguntas-e-Respostas.aspx"
-          target="blank"
-        >
-          Ajuda
-        </Button>
-      </Row>
-    </React.Fragment>
-  );
 };
 
 export default GroupCasesByStatusDashboard;
