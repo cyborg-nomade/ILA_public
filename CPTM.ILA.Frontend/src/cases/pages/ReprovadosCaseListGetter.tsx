61647cec86312c46b95fe16eecc24467eeac2357
Uriel Fiori <uriel.fiori@gmail.com>
2022-06-07 15:09:56 -0300

    - Página Formulário => mostrar botão para editar processos reprovados - Lista reprovados => criar

--- CPTM.ILA.Frontend/src/cases/pages/ReprovadosCaseListGetter.tsx
+++ CPTM.ILA.Frontend/src/cases/pages/ReprovadosCaseListGetter.tsx
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import Spinner from "react-bootstrap/Spinner";
+import Alert from "react-bootstrap/Alert";
+import Row from "react-bootstrap/Row";
+
+import { CaseListItem } from "../../shared/models/DTOs/case-list-item.model";
+import { AuthContext } from "../../shared/context/auth-context";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import CasesList from "../components/CasesList";
 
 const ReprovadosCaseListGetter = () => {
-    return <div>ReprovadosCaseListGetter</div>;
+    const [cases, setCases] = useState<CaseListItem[]>([]);
+
+    const { token, currentGroup } = useContext(AuthContext);
+
+    const { isLoading, error, isWarning, sendRequest, clearError } =
+        useHttpClient();
+
+    useEffect(() => {
+        const getReprovadosCases = async () => {
+            const responseData = await sendRequest(
+                `${process.env.REACT_APP_CONNSTR}/cases/group/${currentGroup.id}/status/false/false/true`,
+                undefined,
+                undefined,
+                { Authorization: "Bearer " + token }
+            );
+            const loadedCases: CaseListItem[] = responseData.cases;
+            console.log("currentGroup: ", currentGroup);
+            console.log("loadedCases: ", loadedCases);
+
+            setCases(loadedCases);
+        };
+
+        getReprovadosCases().catch((error) => {
+            console.log(error);
+        });
+    }, [sendRequest, token, currentGroup]);
+
+    if (isLoading) {
+        return (
+            <Row className="justify-content-center">
+                <Spinner animation="border" role="status" variant="light">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            </Row>
+        );
+    }
+
+    return (
+        <React.Fragment>
+            <h2>
+                Revisar Processos Reprovados - Todos os seus processos
+                reprovados
+            </h2>
+            {error && (
+                <Alert
+                    variant={isWarning ? "warning" : "danger"}
+                    onClose={clearError}
+                    dismissible
+                >
+                    {error}
+                </Alert>
+            )}
+            <CasesList items={cases} redirect={true} />
+        </React.Fragment>
+    );
 };
 
 export default ReprovadosCaseListGetter;
