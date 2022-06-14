ba7a89d1954cf688996e9001c43a9cc6996eda10
Uriel Fiori <uriel.fiori@gmail.com>
2022-06-14 15:29:47 -0300

    - Formulário => show loading on form while data doesn't show

--- CPTM.ILA.Frontend/src/shared/hooks/utilities-hook.tsx
+++ CPTM.ILA.Frontend/src/shared/hooks/utilities-hook.tsx
@@ -3,68 +3,80 @@ import { AuthContext } from "../context/auth-context";
 import { useHttpClient } from "./http-hook";
 
 export const useUtilities = () => {
-  const [systems, setSystems] = useState<string[]>([]);
-  const [countries, setCountries] = useState<string[]>([]);
+    const [systems, setSystems] = useState<string[]>([]);
+    const [countries, setCountries] = useState<string[]>([]);
 
-  const { token } = useContext(AuthContext);
+    const { token } = useContext(AuthContext);
 
-  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-  const getSystems = useCallback(async () => {
-    try {
-      const responseData = await sendRequest(
-        `${process.env.REACT_APP_CONNSTR}/utilities/systems`,
-        undefined,
-        undefined,
-        { "Content-Type": "application/json", Authorization: "Bearer " + token }
-      );
+    const getSystems = useCallback(async () => {
+        try {
+            const responseData = await sendRequest(
+                `${process.env.REACT_APP_CONNSTR}/utilities/systems`,
+                undefined,
+                undefined,
+                {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + token,
+                }
+            );
 
-      const systems: string[] = responseData.systems;
-      console.log("systems: ", systems);
+            const systems: string[] = responseData.systems;
+            console.log("systems: ", systems);
 
-      setSystems(systems);
-    } catch (err) {
-      console.log(err);
-    }
-  }, [sendRequest, token]);
+            setSystems(systems);
+        } catch (err) {
+            console.log(err);
+        }
+    }, [sendRequest, token]);
 
-  const getCountries = useCallback(async () => {
-    try {
-      const responseData = await sendRequest(
-        `${process.env.REACT_APP_CONNSTR}/utilities/countries`,
-        undefined,
-        undefined,
-        { "Content-Type": "application/json", Authorization: "Bearer " + token }
-      );
+    const getCountries = useCallback(async () => {
+        try {
+            const responseData = await sendRequest(
+                `${process.env.REACT_APP_CONNSTR}/utilities/countries`,
+                undefined,
+                undefined,
+                {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + token,
+                }
+            );
 
-      const countries: string[] = responseData.countries;
-      console.log("countries: ", countries);
+            const countries: string[] = responseData.countries;
+            console.log("countries: ", countries);
 
-      setCountries(countries);
-    } catch (err) {
-      console.log(err);
-    }
-  }, [sendRequest, token]);
+            setCountries(countries);
+        } catch (err) {
+            console.log(err);
+        }
+    }, [sendRequest, token]);
 
-  useEffect(() => {
-    const runUtilities = async () => {
-      try {
-        await getSystems();
-        await getCountries();
-      } catch (error) {
-        console.log(error);
-      }
+    useEffect(() => {
+        const runUtilities = async () => {
+            try {
+                await getSystems();
+                await getCountries();
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        runUtilities().catch((error) => {
+            console.log(error);
+        });
+
+        return () => {
+            setCountries([]);
+            setSystems([]);
+        };
+    }, [getCountries, getSystems]);
+
+    return {
+        systems,
+        countries,
+        isLoadingUtilities: isLoading,
+        error,
+        clearError,
     };
-
-    runUtilities().catch((error) => {
-      console.log(error);
-    });
-
-    return () => {
-      setCountries([]);
-      setSystems([]);
-    };
-  }, [getCountries, getSystems]);
-
-  return { systems, countries, isLoading, error, clearError };
 };
