f572984462bf2671c8c157ccd76f3c8d243f76d1
Uriel Fiori <uriel.fiori@gmail.com>
2022-06-19 10:20:51 -0300

    - Formulário => trata check if clicked (SECTION 11)

--- CPTM.ILA.Frontend/src/cases/components/form-items/Section11FormRow.tsx
+++ CPTM.ILA.Frontend/src/cases/components/form-items/Section11FormRow.tsx
@@ -13,107 +13,140 @@ import CreateCommentBox from "../../../threads-comments/components/CreateComment
 import NewSection11FormRowSub from "./Section11FormRowSub";
 
 const Section11FormRow = (props: {
-  disabled: boolean;
-  methods: UseFormReturn<Case>;
+    disabled: boolean;
+    methods: UseFormReturn<Case>;
+    radioCheckedHandler: (radioChackedName: string) => void;
+    isNew: boolean;
 }) => {
-  const { fields, append, remove } = useFieldArray({
-    control: props.methods.control, // control props comes from useForm
-    name: "compartilhamentoDadosPessoais" as const, // unique name for your Field Array
-  });
+    const { fields, append, remove } = useFieldArray({
+        control: props.methods.control, // control props comes from useForm
+        name: "compartilhamentoDadosPessoais" as const, // unique name for your Field Array
+    });
 
-  const [trata, setTrata] = useState(false);
+    const [trata, setTrata] = useState("INVALID");
 
-  const handleTrataRadio = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.currentTarget.value === "SIM") {
-      setTrata(true);
-    } else {
-      setTrata(false);
-      props.methods.setValue("compartilhamentoDadosPessoais", []);
-    }
-  };
+    const handleTrataRadio = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTrata(event.currentTarget.value);
+        if (event.currentTarget.value !== "INVALID") {
+            props.radioCheckedHandler("compartilhamentoDadosPessoais");
+        }
+        if (event.currentTarget.value === "NÃO") {
+            props.methods.setValue("compartilhamentoDadosPessoais", []);
+        }
+    };
 
-  return (
-    <React.Fragment>
-      <Row>
-        <Col className="d-grid justify-content-center">
-          <Form.Check
-            type="radio"
-            name={`trata-compartilhamentoDadosPessoais`}
-            required
-            label="Sim"
-            value="SIM"
-            checked={trata}
-            disabled={props.disabled}
-            onChange={handleTrataRadio}
-          />
-          <Form.Check
-            type="radio"
-            name={`trata-compartilhamentoDadosPessoais`}
-            required
-            inline
-            label="Não"
-            value="NÃO"
-            checked={!trata}
-            disabled={props.disabled}
-            onChange={handleTrataRadio}
-          />
-        </Col>
-        <Form.Label as={Col}></Form.Label>
-        <Form.Label as={Col}></Form.Label>
-        <Form.Label as={Col}></Form.Label>
-        <Form.Label as={Col}></Form.Label>
-        <Form.Label as={Col}></Form.Label>
-        <Col lg={1}>
-          <Row>
-            <CreateCommentBox
-              item={CaseIndexDictionary.compartilhamentoDadosPessoais}
-            />
-          </Row>
-        </Col>
-      </Row>
-      <React.Fragment>
-        {fields && fields.length > 0 ? (
-          fields.map((field, index) => (
-            <React.Fragment key={field.id}>
-              <NewSection11FormRowSub
-                className={`mb-3 pt-2 pb-2 ${
-                  index % 2 === 0 ? "bg-primary bg-opacity-10" : ""
-                }`}
-                disabled={props.disabled}
-                name={`compartilhamentoDadosPessoais[${index}]`}
-                methods={props.methods}
-              />
-              <Row className="justify-content-center">
-                <ButtonGroup as={Col} className="mt-1 mb-3" lg={2}>
-                  <Button
-                    variant="primary"
-                    onClick={() => append(emptyItemCompatilhamentoDados())}
-                  >
-                    +
-                  </Button>
-                  <Button variant="danger" onClick={() => remove(index)}>
-                    -
-                  </Button>
-                </ButtonGroup>
-              </Row>
+    return (
+        <React.Fragment>
+            <Row>
+                <Col className="d-grid justify-content-center">
+                    <Form.Check
+                        type="radio"
+                        name={`trata-compartilhamentoDadosPessoais`}
+                        required
+                        label="Sim"
+                        value="SIM"
+                        checked={trata === "SIM"}
+                        disabled={props.disabled}
+                        onChange={handleTrataRadio}
+                        isInvalid={trata === "INVALID"}
+                    />
+                    <Form.Check
+                        type="radio"
+                        name={`trata-compartilhamentoDadosPessoais`}
+                        required
+                        inline
+                        label="Não"
+                        value="NÃO"
+                        checked={trata === "NÃO"}
+                        disabled={props.disabled}
+                        onChange={handleTrataRadio}
+                        isInvalid={trata === "INVALID"}
+                    />
+                    <Form.Check
+                        type="radio"
+                        name={`trata-compartilhamentoDadosPessoais`}
+                        required
+                        inline
+                        value="INVALID"
+                        checked={trata === "INVALID"}
+                        disabled={props.disabled}
+                        onChange={handleTrataRadio}
+                        style={{ display: "none" }}
+                    />
+                </Col>
+                <Form.Label as={Col}></Form.Label>
+                <Form.Label as={Col}></Form.Label>
+                <Form.Label as={Col}></Form.Label>
+                <Form.Label as={Col}></Form.Label>
+                <Form.Label as={Col}></Form.Label>
+                <Col lg={1}>
+                    <Row>
+                        <CreateCommentBox
+                            item={
+                                CaseIndexDictionary.compartilhamentoDadosPessoais
+                            }
+                        />
+                    </Row>
+                </Col>
+            </Row>
+            <React.Fragment>
+                {fields && fields.length > 0 ? (
+                    fields.map((field, index) => (
+                        <React.Fragment key={field.id}>
+                            <NewSection11FormRowSub
+                                className={`mb-3 pt-2 pb-2 ${
+                                    index % 2 === 0
+                                        ? "bg-primary bg-opacity-10"
+                                        : ""
+                                }`}
+                                disabled={props.disabled}
+                                name={`compartilhamentoDadosPessoais[${index}]`}
+                                methods={props.methods}
+                            />
+                            <Row className="justify-content-center">
+                                <ButtonGroup
+                                    as={Col}
+                                    className="mt-1 mb-3"
+                                    lg={2}
+                                >
+                                    <Button
+                                        variant="primary"
+                                        onClick={() =>
+                                            append(
+                                                emptyItemCompatilhamentoDados()
+                                            )
+                                        }
+                                    >
+                                        +
+                                    </Button>
+                                    <Button
+                                        variant="danger"
+                                        onClick={() => remove(index)}
+                                    >
+                                        -
+                                    </Button>
+                                </ButtonGroup>
+                            </Row>
+                        </React.Fragment>
+                    ))
+                ) : (
+                    <Row className="justify-content-center">
+                        <ButtonGroup as={Col} className="mt-1 mb-3" lg={2}>
+                            <Button
+                                variant="primary"
+                                onClick={() =>
+                                    append(emptyItemCompatilhamentoDados())
+                                }
+                                disabled={!trata}
+                            >
+                                +
+                            </Button>
+                        </ButtonGroup>
+                    </Row>
+                )}
             </React.Fragment>
-          ))
-        ) : (
-          <Row className="justify-content-center">
-            <ButtonGroup as={Col} className="mt-1 mb-3" lg={2}>
-              <Button
-                variant="primary"
-                onClick={() => append(emptyItemCompatilhamentoDados())}
-                disabled={!trata}
-              >
-                +
-              </Button>
-            </ButtonGroup>
-          </Row>
-        )}
-      </React.Fragment>
-    </React.Fragment>
-  );
+        </React.Fragment>
+    );
 };
 
 export default Section11FormRow;
